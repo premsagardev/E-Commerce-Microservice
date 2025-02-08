7f6d6f7b31be14ae202ea134e5c6bd1dfeba27d0
premsagars.1811@gmail.com <premsagars.1811@gmail.com>
2025-02-09 00:09:13 +0530

    initial commit

---
+++ src/test/java/com/example/E_commerce_microservice/ECommerceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_commerce_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
